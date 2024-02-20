398d926bbbe6c6aa6eaa5390d3a54806ab492efc
teb <85996160+Teebak@users.noreply.github.com>
2024-02-20 19:15:02 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+function wrapGifts(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(`Wrapped ${array[i]} and added a bow!`);
+        
+    }
+}
+
+
+
+function writeCards(array, event){
+    let newaraay = [];
+    for (let i = 0; i < array.length; i++) {
+        newaraay.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newaraay;
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
